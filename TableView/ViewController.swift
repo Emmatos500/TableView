b262fd1af705d93bcc1502393273cee387875b7f
Emmett <ekennedy@cps.edu>
2021-07-27 12:28:57 -0500

    TableView

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
 
+    @IBOutlet weak var tableViewCell: UITableView!
+    
+    
+
+    
+
+
+        var items = ["Gummy Bears", "Eggs", "Steak"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableViewCell.dataSource = self
+        tableViewCell.delegate = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int{
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell1 =  tableView.dequeueReusableCell(withIdentifier: "cell1")
+        cell1?.textLabel?.text = items[indexPath.row]
+        
+        return cell1!
+    }
 }
 
